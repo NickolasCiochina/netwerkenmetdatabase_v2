14da3ee02d9cb4abac9b2790dd5e9c681ee75a16
Nickolas Ciochina <nickolas.ciochina@student.graafschapcollege.nl>
2024-03-19 11:41:39 +0100

    email add

---
+++ email.php
@@ -0,0 +1,19 @@
+ // these values are usualy filled by a form
+        $subject = "Hello World"
+        $message = "Dit is het eerste mail bericht via PHP"
+        $email = "nickolas.ciochina@student.graafschapcollege.nl" // sender mail address use your own!
+
+        // Recipient email address
+        $to = '<p.veltman>@<graafschapcollege.nl>.gc-webhosting.nl';
+        
+        // Create email headers
+        $headers = 'From: '. $email . "\r\n" .
+        'Reply-To: '. $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+        
+        // Sending email
+        if(mail($to, $subject, $message, $headers)){
+            echo '<p class="success">Your message has been sent successfully!</p>';
+        } else{
+            echo '<p class="error">Unable to send email. Please try again!</p>';
+        }
